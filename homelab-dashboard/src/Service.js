599ee1056d9daff02b13b56d9a6c78e771b723e3
Ben Kilkowski <benkilkowski@outlook.com>
2024-01-14 20:54:10 -0600

    Services Styling and Flex

--- homelab-dashboard/src/Service.js
+++ homelab-dashboard/src/Service.js
@@ -1,12 +1,10 @@
 function Service( {imgPath, serviceName, ipAddress, hostName} ) {
     return (
-      <a id="services-container" href="#">
-          <div className="service-container">
-            <span><img src={imgPath}></img></span>
-            <div>
-              <h3>{serviceName}</h3>
-              <p>{ipAddress} | {hostName}</p>
-            </div>
+      <a id="service-container" href="#">
+          <span><img src={imgPath}></img></span>
+          <div>
+            <h3>{serviceName}</h3>
+            <p>{ipAddress} | {hostName}</p>
           </div>
       </a>
     );
--- homelab-dashboard/src/ServicesSection.js
+++ homelab-dashboard/src/ServicesSection.js
@@ -4,24 +4,26 @@ function ServicesSection() {
     return (
         <div id="services-section">
             <h2>Services</h2>
-            <Service 
-                imgpath={"#"}
-                serviceName={"Proxmox VE"}
-                ipAddress={"###.###.###.###"}
-                hostName={"proxmox"}
-            />
-            <Service 
-                imgpath={"#"}
-                serviceName={"Open Media Vault - NAS"}
-                ipAddress={"###.###.###.###"}
-                hostName={"omv-nas"}
-            />
-            <Service 
-                imgpath={"#"}
-                serviceName={"Supermico IMPI"}
-                ipAddress={"###.###.###.###"}
-                hostName={"smc-server"}
-            />
+            <div id="services-container">
+                <Service 
+                    imgpath={"#"}
+                    serviceName={"Proxmox VE"}
+                    ipAddress={"###.###.###.###"}
+                    hostName={"proxmox"}
+                />
+                <Service 
+                    imgpath={"#"}
+                    serviceName={"Open Media Vault - NAS"}
+                    ipAddress={"###.###.###.###"}
+                    hostName={"omv-nas"}
+                />
+                <Service 
+                    imgpath={"#"}
+                    serviceName={"Supermico IMPI"}
+                    ipAddress={"###.###.###.###"}
+                    hostName={"smc-server"}
+                />
+            </div>
         </div>
     );
 }
